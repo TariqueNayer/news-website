912837c3d61081ac58e26d9b7d50e0c63a721acb
TariqueNayer <tariquenayer2476@gmail.com>
2025-08-03 13:32:46 +0530

    more errors

--- django_project/wsgi.py
+++ django_project/wsgi.py
@@ -13,12 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_project.settings")
 
-try:
-    from django.core.management import call_command
-    call_command('migrate', interactive=False)
-    call_command('collectstatic', interactive=False, verbosity=2)
-except Exception as e:
-    print(f"Startup command error: {e}")
 
 application = get_wsgi_application()
 
